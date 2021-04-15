522d18e34ca7bc8331bc10928eb7a148503e233c
Junaed Rayhan <junaedrayhan@gmail.com>
2021-04-15 16:31:29 +0600

    done header section

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -4,6 +4,25 @@ import './Banner.css'
 const Banner = () => {
     return (
         <div className="banner-section">
+            <div className="gender-card">
+                <div className="gender">
+                    <h2>Gender</h2>
+                    <ul>
+                        <li><a href="/male">Male</a></li>
+                        <li><a href="/female">Female</a></li>
+                    </ul>
+                </div>
+                <div className="appointment">
+                    <h2>Book Appointment</h2>
+                    <ul>
+                        <li><a href="/service">Home Service</a></li>
+                        <li><a href="/appointment">Appointment</a></li>
+                    </ul>
+                </div>
+                <div className="empty">
+
+                </div>
+            </div>
             <CarouselItems/>
         </div>
     );
--- src/Components/Home/Menubar/Menubar.js
+++ src/Components/Home/Menubar/Menubar.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './Menubar.css'
+import profile from '../../../images/profile.jpg'
+import menuIcon from '../../../images/menu-bar.png'
 import {NavDropdown} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import brand from '../../../images/brand.png'
 const Menubar = () => {
+    const closeNav = () => {
+        document.getElementById('menu').style.visibility = 'hidden';
+
+    }
+    const openNav = () => {
+        document.getElementById('menu').style.visibility = 'visible'
+    }
     return (
+        <>
         <div className="navbar-wrapper">
+            <div className="row container-fluid hamburger-section">
+                <div className="hamburger-icon">
+                    <a href="javascript:void(0)" onClick={openNav} class="openBtn">
+                        <img src={menuIcon}  alt=""/>
+                    </a>
+                </div>
+                <div className="hamburger-profile">
+                    <img src={profile} alt="" className="profile"/>
+                </div>
+            </div>
+            
             <div className="brand">
                 <a href="">
                     <img src={brand} alt="" height="35px" width="40px" className="img-responsive"/>
@@ -43,9 +64,25 @@ const Menubar = () => {
                     <li><a href="">Blog</a></li>
                     <li><a href="">Refer</a></li>
                     <li><a href="" className="login">Login/signup</a></li>
-                </ul>
+                </ul>  
             </div>
         </div>
+        <div className="mobile-menu" id="menu" style={{visibility: 'hidden'}}>
+            <a href="javascript:void(0)" onClick={closeNav} class="closeBtn"><span>&times;</span></a>
+            <ul>
+                <li><a onClick={closeNav} href="/home">Home</a></li>
+                <li><a onClick={closeNav} href="/offer">Offer</a></li>
+                <NavDropdown title="Dropdown" id="collasible-nav-dropdown" style={{backgroundColor: 'transparent', marginLeft : '-15px',marginBottom : '20px'}}>
+                    <NavDropdown.Item onClick={closeNav} href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item onClick={closeNav} href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item onClick={closeNav} href="#action/3.3">Something</NavDropdown.Item>
+                </NavDropdown>
+                <li><a onClick={closeNav} href="">Blog</a></li>
+                <li><a onClick={closeNav} href="">Refer</a></li>
+                <li><a onClick={closeNav} href="" className="login">Login/signup</a></li>
+            </ul>   
+        </div>
+        </>
     );
 };
 
